b2774747bdc96e82802f10fbb766a0f64012afd7
khang <tgkhang22@clc.fitus.edu.vn>
2025-06-20 02:18:19 +0700

    [+] NDS WEather snapshot table and location table

--- scripts/4_GOLD (nds)/6_driver_type.sql
+++ scripts/4_GOLD (nds)/6_driver_type.sql
@@ -3,32 +3,26 @@ CREATE TABLE NDS.Driver_Type (
     race NVARCHAR(100) NULL,            
     gender NVARCHAR(50) NULL,
     driver_state NVARCHAR(50) NULL,
-    driver_city NVARCHAR(50) NULL,
+    driver_city_id NVARCHAR(50) NULL,
     commercial_license BIT NULL         
 );
 GO
 
 
-select distinct
+SELECT DISTINCT
     CASE 
         WHEN driver_state IS NULL THEN 'n/a'
         ELSE driver_state
     END AS driver_state, 
-    UPPER(LEFT(race, 1)) + LOWER(SUBSTRING(race, 2, LEN(race))) as race,
-    commercial_license,
-	CASE WHEN UPPER(TRIM(gender)) = 'M' then 'Male'
-		WHEN UPPER(TRIM(gender)) = 'F' then 'Female'
-		else 'n/a'	
-	END gender,
-    CASE 
-        WHEN driver_city IS NULL THEN 'n/a'
-        WHEN LTRIM(RTRIM(driver_city)) IN ('', '.', '00', '000') THEN 'n/a'
-        WHEN PATINDEX('%[^a-zA-Z0-9 ]%', driver_city) > 0 THEN 'n/a'
-        WHEN ISNUMERIC(driver_city) = 1 THEN 'n/a'
-        WHEN PATINDEX('[0-9]%', driver_city) = 1 THEN 'n/a'
-        ELSE UPPER(LEFT(driver_city, 1)) + LOWER(SUBSTRING(driver_city, 2, LEN(driver_city)))
-    END AS city
-from Staging_Violation
-where driver_state is null
 
+    race,
+    commercial_license,
+    CASE 
+        WHEN UPPER(TRIM(gender)) = 'M' THEN 'Male'
+        WHEN UPPER(TRIM(gender)) = 'F' THEN 'Female'
+        ELSE 'n/a'
+    END AS gender,
+    c.city_id
+FROM Bronze.Violation v
+join NDS.City c on c.city_name= v.driver_city
 
